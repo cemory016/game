2cbb1a29c5f8f14ac2dcd56053676e09e76241c8
Carmen <cemory016@gmail.com>
2018-02-06 14:51:53 -0500

    I broke it. sad face

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 console.log ("i'm working")
-function pts() {
-    var questionC1 = prompt("____'s the way (uh huh, uh huh) I like it");
-    if (questionC1 === "That") {
-        document.getElementById("answerC1").innerHTML =
+function qANDa() {
+    var answer1 = prompt("____'s the way (uh huh, uh huh) I like it");
+    if (questionC1R1 === "that") {
+        document.getElementById("answer1").innerHTML =
         "correct";
     } else{
-        document.getElementById("answerC1").innerHTML =
+        document.getElementById("answer1").innerHTML =
         "next player";
     }
-}
+    var questionC2R1 = prompt("...knick-knack paddywhack, give the dog a bone, ____ old man came rolling home”);
+    if (questionC2R1 === "this") {
+        document.getElementById("answer2").innerHTML =
+        "correct";
+    } else{
+        document.getElementById("answer2").innerHTML =
+        "next player";
+    }}
