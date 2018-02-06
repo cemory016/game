172081bba3b8bd3005788f11be2cff6e4aae6904
Carmen <cemory016@gmail.com>
2018-02-06 08:12:16 -0500

    prompt button with response

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log ("i'm working")
+function pts() {
+    var questionC1 = prompt("This will be the question");
+    if (questionC1 != null) {
+        document.getElementById("answerC1").innerHTML =
+        "if right you get pts if wrong next player";
+    }
+}
