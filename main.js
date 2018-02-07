b41cccddcb74b4ee984f969f841997259dab6541
Carmen <cemory016@gmail.com>
2018-02-06 23:45:29 -0500

    took a nap and then fixed buttons

--- main.js
+++ main.js
@@ -1,18 +1,83 @@
 console.log ("i'm working")
 function qANDa() {
     var answer1 = prompt("____'s the way (uh huh, uh huh) I like it");
-    if (questionC1R1 === "that") {
+    if (answer1 === "that") {
         document.getElementById("answer1").innerHTML =
         "correct";
     } else{
         document.getElementById("answer1").innerHTML =
         "next player";
+    };
+    var answer2 = prompt("...knick-knack paddywhack, give the dog a bone, ____ old man came rolling home");
+    if (answer2 === "this") {
+        document.getElementById("answer2").innerHTML =
+        "correct";
+    } else{
+        document.getElementById("answer2").innerHTML =
+        "next player";
+    };
+    var answer3 = prompt("All ____");
+    if (answer3 === "that") {
+        document.getElementById("answer3").innerHTML =
+        "correct";
+    } else{
+        document.getElementById("answer3").innerHTML =
+        "next player";
+    };
+    var answer4 = prompt("To be, or not to be, ____ is the question...");
+    if (answer4 === "that") {
+        document.getElementById("answer4").innerHTML =
+        "correct";
+    } else{
+        document.getElementById("answer4").innerHTML =
+        "next player";
+    };
+    var answer5 = prompt("Th-th-th-____'s all folks!");
+    if (answer5 === "that") {
+        document.getElementById("answer5").innerHTML =
+        "correct";
+    } else{
+        document.getElementById("answer5").innerHTML =
+        "next player";
+    };
+        var answer6 = prompt("____'s the way (uh huh, uh huh) I like it");
+        if (answer6 === "that") {
+            document.getElementById("answer6").innerHTML =
+            "correct";
+        } else{
+            document.getElementById("answer6").innerHTML =
+            "next player";
+        };
+        var answer7 = prompt("...knick-knack paddywhack, give the dog a bone, ____ old man came rolling home");
+        if (answer7 === "this") {
+            document.getElementById("answer7").innerHTML =
+            "correct";
+        } else{
+            document.getElementById("answer7").innerHTML =
+            "next player";
+        };
+        var answer8 = prompt("All ____");
+        if (answer8 === "that") {
+            document.getElementById("answer8").innerHTML =
+            "correct";
+        } else{
+            document.getElementById("answer8").innerHTML =
+            "next player";
+        };
+        var answer9 = prompt("To be, or not to be, ____ is the question...");
+        if (answer9 === "that") {
+            document.getElementById("answer9").innerHTML =
+            "correct";
+        } else{
+            document.getElementById("answer9").innerHTML =
+            "next player";
+        };
+        var answer10 = prompt("Th-th-th-____'s all folks!");
+        if (answer10 === "that") {
+            document.getElementById("answer10").innerHTML =
+            "correct";
+        } else{
+            document.getElementById("answer10").innerHTML =
+            "next player";
+        };
     }
-    var questionC2R1 = prompt("...knick-knack paddywhack, give the dog a bone, ____ old man came rolling home”);
-    if (questionC2R1 === "this") {
-        document.getElementById("answer2").innerHTML =
-        "correct";
-    } else{
-        document.getElementById("answer2").innerHTML =
-        "next player";
-    }}
